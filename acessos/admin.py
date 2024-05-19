737428ff02d5ba6aab11f2348795f565fc342836
Igor Carvalho <igorcodigo0@gmail.com>
2024-05-19 03:26:19 -0300

    Mascarando parte do ip no painel admin

--- acessos/admin.py
+++ acessos/admin.py
@@ -3,7 +3,18 @@ from .models import RateLimit
 
 @admin.register(RateLimit)
 class RateLimitAdmin(admin.ModelAdmin):
-    list_display = ('ip_address', 'request_count', 'last_request')
+    list_display = ('masked_ip_address', 'request_count', 'last_request')
     list_filter = ('ip_address', 'last_request')
     search_fields = ('ip_address',)
     ordering = ('-last_request',)
+
+    def masked_ip_address(self, obj):
+        # Dividimos o endereço IP em partes e mascaramos os últimos números
+        parts = obj.ip_address.split('.')
+        if len(parts) == 4:
+            return f"{parts[0]}.***.***.***"
+        else:
+            # Caso o endereço IP não esteja no formato IPv4
+            return "Invalid IP"
+
+    masked_ip_address.short_description = 'IP Address'
