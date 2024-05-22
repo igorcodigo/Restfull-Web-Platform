049a290e5b81b54e2cdeeea46768e45a6ed8ef5f
Igor Carvalho <igorcodigo0@gmail.com>
2024-05-22 13:26:19 -0300

    Mudancas nos nomes dos endpoints de login e mudancas para permitir que possam ser registrados usuarios mesmo sem estar logado

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -11,7 +11,7 @@ router.register(r'users', CustomUserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/login/refreshtoken/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,25 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
-    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        else:
+            self.permission_classes = [IsAuthenticated]
+        return super(CustomUserViewSet, self).get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        user.set_password(request.data['password'])
+        user.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
