bbf272bd3fce2588d7d4a51a1f86864ed978038a
Maurício "Conde" Ramon <mcondero@hotmail.com>
2024-06-03 10:16:49 -0300

    feature: modifying string to match native language

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BookListCreate, BookRetrieveUpdateDestroy
 
 urlpatterns = [
-    path('books/', BookListCreate.as_view(), name='book-list-create'),
+    path('books/', BookListCreate.as_view(), name='criar-lista-livros'),
     path('books/<int:pk>/', BookRetrieveUpdateDestroy.as_view(), name='book-retrieve-update-destroy'),
 ]
