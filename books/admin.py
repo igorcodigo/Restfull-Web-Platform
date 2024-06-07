4761b19d7f1e0fb47f63d25f49d6cdee7b3b6558
Igor Carvalho <igorcodigo0@gmail.com>
2024-06-06 22:11:45 -0300

    Registro de books no painel de admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'price', 'category')
+    search_fields = ('title', 'author', 'category')
+    list_filter = ('category', 'author')
+
+# Alternativamente, você pode registrar diretamente sem o decorador
+# admin.site.register(Book, BookAdmin)
